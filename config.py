7554791b15f8cb5f54626b6e73807be7785c1d93
Xafter <xafter@icloud.com>
2018-06-11 21:14:56 +0800

    添加config注释

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
-raw_data_path = './input/iptv_new_zxrecord_20180418.txt'
-processed_path = './processed_data'
-cleaned_path = processed_path + '/cleaned_data.csv'
-text_info_path = './text_info/text_info.csv'
-result_path = './result'
-cluster_result_path = result_path + '/cluster_result.pkl'# dataframe uid, label
+raw_data_path = './input/iptv_new_zxrecord_20180418.txt'# 原文件
+processed_path = './processed_data' # 处理之后的文件
+cleaned_path = processed_path + '/cleaned_data.csv' # 清洗之后的数据
+text_info_path = './text_info/text_info.csv' # 节目对应的标签
+result_path = './result'# 结果
+cluster_result_path = result_path + '/cluster_result.pkl'# 聚类结果
